254313f8c6ccc3466b3d7336351f718d8598bded
mihnead01 <71493102+mihnead01@users.noreply.github.com>
2024-04-22 16:49:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const car = document.getElementById("car");
+
+function startAnimation() {
+  car.style.transform = "translateX(500px)";
+}
+
+function resetAnimation() {
+  car.style.transform = "translateX(0)";
+}
