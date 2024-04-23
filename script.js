c3b88907942c1fb8bfef7b5880094921b23509df
mihnead01 <71493102+mihnead01@users.noreply.github.com>
2024-04-23 11:15:18 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const car = document.getElementById("car");
-
-function startAnimation() {
-  car.style.transform = "translateX(500px)";
-}
-
-function resetAnimation() {
-  car.style.transform = "translateX(0)";
-}
