44ae6ab8a9dc20282c754751f41e3ac7133ca8d9
mihnead01 <71493102+mihnead01@users.noreply.github.com>
2026-01-12 17:43:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,6 @@ function startAnimation() {
 function resetAnimation() {
   car.style.transform = "translateX(0)";
 }
+function flyAnimation() {
+  car.style.transform = "translate(800px, -300px)";
+}
